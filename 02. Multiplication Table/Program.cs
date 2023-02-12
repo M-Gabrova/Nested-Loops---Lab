78927bd2a272b43d7f39f5e214c874243299f6a1
M-Gabrova <121133276+M-Gabrova@users.noreply.github.com>
2023-02-12 14:33:55 +0200

    Update Program.cs

--- 02. Multiplication Table/Program.cs
+++ 02. Multiplication Table/Program.cs
@@ -6,9 +6,9 @@ namespace _01._Clock
     {
         static void Main(string[] args)
         {
-            for (int x = 0; x <= 10; x++)
+            for (int x = 1; x <= 10; x++)
             {
-                for (int y = 0; y <= 10; y++)
+                for (int y = 1; y <= 10; y++)
                 {
                     int product = x * y;
                     Console.WriteLine($"{x} * {y} = {product}"); 
